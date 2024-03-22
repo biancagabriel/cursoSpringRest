a0d2b295d09b41935e5a40473c812883f7ac5e8a
Bianca Gabriel <71979485+biancagabriel@users.noreply.github.com>
2024-03-22 19:07:55 -0300

    alteração de pom para formato xml e função de cliente

---
+++ awpag-api/awpag-api/src/main/java/com/cursoSprig/awpag/api/controller/ClienteController.java
@@ -0,0 +1,28 @@
+package com.cursoSprig.awpag.api.controller;
+
+import com.cursoSprig.awpag.domain.model.Cliente;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class ClienteController {
+    @GetMapping("/clientes")
+        public List<Cliente> listar(){
+            var cliente1 = new Cliente();
+            cliente1.setId(1L);
+            cliente1.setNome("joão");
+            cliente1.setTelefone("11 99999-9999");
+            cliente1.setEmail("joao@blabla.com");
+
+            var cliente2 = new Cliente();
+            cliente2.setId(2L);
+            cliente2.setNome("maria");
+            cliente2.setTelefone("11 99999-9900");
+            cliente2.setEmail("maria@blabla.com");
+
+            return Arrays.asList(cliente1,cliente2);
+        }
+}
